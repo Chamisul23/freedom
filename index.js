52afc5ef0b7a5f57a8883a017c1f692e9b9f750c
17202687 <b0522033@gmail.com>
2022-01-11 16:55:44 +0900

    first upload

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+// first running file
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
